2f955eb78e341c5b84049498e179d119df3b0396
Cameron <cameron.cw@outlook.com>
2025-08-11 15:04:18 +0100

    Nav Bar finished. Making Start on first section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <h1 className="text-6xl text-purple-500"> Hello World!</h1>
+    <div className="row">
+      <div className="justify-center content-center px-10">
+        <h1 className="font-bold text-6xl">Your <span className="blue">Launchpad</span> to an AI-Driven Future</h1>
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="row">
       <Link href="/">
-        <Image src="/assets/Logo_Light.png" alt="Warburton's AI Solutions Ltd Logo" width={120} height={40} />
+        <Image src="/assets/Logo_No_Background.png" alt="Warburton's AI Solutions Ltd Logo" width={200} height={200} className="nav__logo"/>
       </Link>
       <ul className="nav__links">
         <li className="nav__link">
