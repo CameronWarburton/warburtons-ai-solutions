fc5573ed92a1c169bd4d38d7d685efc314b910cd
Cameron <cameron.cw@outlook.com>
2025-09-29 21:42:03 +0100

    Finished About Us Section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Services />
       <About />
+      <Contact />
     </main>
   );
 }
